c3fa304ed7a7e668745c462aff36ff07dcd461c1
philer <philipp.miller@t-online.de>
2018-04-03 05:51:53 +0200

    fixed missing line numbers on syntax error

--- src/highlight.js
+++ src/highlight.js
@@ -35,19 +35,18 @@ ttClasses[TT.INTEGER] =  "number";
 export default function highlight(text) {
   let html = "";
   let lineno = 1;
+  const lineSep = () => `</span></span><span class="line"><span class="lineno">${++lineno}</span><span>`;
   const tokens = new TokenIterator(text, true);
   try {
     for (const token of tokens) {
       if (token.type in ttClasses) {
         html += `<span class="token ${ttClasses[token.type]}">${token.value}</span>`;
       } else {
-        html += token.value.replace(/\n/g, function() {
-          return `</span></span><span class="line"><span class="lineno">${++lineno}</span><span>`;
-        });
+        html += token.value.replace(/\n/g, lineSep);
       }
     }
   } catch (err) {
-    html += tokens.remainingText;
+    html += tokens.remainingText.replace(/\n/g, lineSep);
   }
   return `<span class="line"><span class="lineno">1</span><span>${html}</span></span>`;
 }
