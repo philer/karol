21f3a3ec56e0cd47230b066262e9af0a778b1f67
philer <philipp.miller@t-online.de>
2018-04-03 17:31:35 +0200

    editor: custom caret, added monokai syntax highlighting theme

--- build/core.js
+++ build/core.js
@@ -995,11 +995,21 @@
       this.textarea = root.getElementsByClassName("editor-textarea")[0];
       this.highlighted = root.getElementsByClassName("editor-highlight")[0];
       this.scrollbox = root.getElementsByClassName("editor-scrollbox")[0];
+      this.caretLayer = root.getElementsByClassName("editor-caret-layer")[0];
+      this.caret = document.createElement("span");
+      this.caret.classList.add("editor-caret");
 
       this.textarea.addEventListener("input", this.update.bind(this));
       //  more events: paste propertychange
 
+      const updateCaret = this.updateCaret.bind(this);
+      const deferredCaretUpdate = () => setTimeout(updateCaret, 0);
+      this.textarea.addEventListener("click", deferredCaretUpdate);
+      this.textarea.addEventListener("input", deferredCaretUpdate);
+      this.textarea.addEventListener("keypress", deferredCaretUpdate);
+
       this.update();
+      updateCaret();
     }
 
     get value() {
@@ -1013,6 +1023,21 @@
     update() {
       this.highlighted.innerHTML = highlight(this.textarea.value);
       this.textarea.style.height = this.highlighted.offsetHeight + "px";
+    }
+
+    updateCaret() {
+      this.caretLayer.innerHTML = this.textarea.value;
+      this.caretLayerText = this.caretLayer.firstChild;
+      if (this.caretLayerText) {
+        const offset = this.textarea.selectionDirection === "forward"
+                     ? this.textarea.selectionEnd
+                     : this.textarea.selectionStart;
+        const range = document.createRange();
+        range.setStart(this.caretLayerText, offset);
+        range.insertNode(this.caret);
+      } else {
+        this.caretLayer.appendChild(this.caret);
+      }
     }
 
     // gotoLine(lineno) {
