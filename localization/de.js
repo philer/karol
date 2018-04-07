bf0bcfa147777d239ef58a121dcbef84f14ee4da
philer <philipp.miller@t-online.de>
2018-04-07 19:58:08 +0200

    added german translation for error messages (Danke Lars :D)

--- localization/de.js
+++ localization/de.js
@@ -27,4 +27,38 @@ config({
     flat: "Flache Welt",
     show_player: "Figur anzeigen",
   },
+  error: {
+    parser: {
+      token_read:
+        "Syntax-Fehler in Zeile {line}, Spalte {column}: Nächstes Wort nicht lesbar.",
+      unexpected_token:
+        "Lese-Fehler in Zeile {line}, Spalte {column}: Unerwartes Wort {token}.",
+      unexpected_token_instead:
+        "Lese-Fehler in Zeile {line}, Spalte {column}: Unerwartes Wort {token}, erwarte stattdessen {expected} .",
+      nested_program_definition:
+        "Lese-Fehler in Zeile {}: Programm in verschachteltem Kontext nicht definierbar.",
+      nested_routine_definition:
+        "Lese-Fehler in Zeile {}: Anweisung in verschachteltem Kontext nicht definierbar.",
+    },
+    runtime: {
+      undefined: "Laufzeit-Fehler in Zeile {line}: {identifier} nicht definiert.",
+      max_recursion_depth_exceeded:
+        "Laufzeit-Fehler: Maximales Rekursionstiefe ({}) überschritten.",
+      unimplemented_statement_type:
+        "Laufzeit-Fehler: Nicht implementierter Anweisungstyp {type}.",
+      unimplemented_expression_type:
+        "Laufzeit-Fehler: Nicht implementierter Ausdrucksstyp {type}.",
+    },
+    world: {
+      move_out_of_world: "Ungültige Bewegung: außerhalb der Welt",
+      jump_too_high: "Ungültige Bewegung: jump too high",
+      move_cuboid: "Ungültige Bewegung: cuboid",
+      action_out_of_world: "Ungültige Aktion: außerhalb der Welt",
+      action_cuboid: "Ungültige Aktion: Block auf Quader",
+      action_too_high: "Ungültige Aktion: Baut zu hoch",
+      action_no_blocks: "Ungültige Aktion: keine Blöcke",
+      action_already_marked: "Ungültige Aktion: bereits markiert",
+      action_no_mark: "Ungültige Aktion: nicht markiert",
+    },
+  },
 });
