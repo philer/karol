466f43770fba614488ab79d36ad59ffeabf30404
philer <philipp.miller@t-online.de>
2021-12-08 01:22:58 +0100

    Fix invisible Popovers overlaying controls

--- src/ui/Appear.tsx
+++ src/ui/Appear.tsx
@@ -1,4 +1,5 @@
 import {JSX, VNode, cloneElement} from "preact"
+import {useEffect, useState} from "preact/hooks"
 
 import type {DivProps} from "../util/types"
 
@@ -19,12 +20,40 @@ const visible = {
   transform: "none",
 }
 
-export const Appear = ({children, show}: AppearProps) =>
-  cloneElement(children, {
+export const Appear = ({children, show}: AppearProps) => {
+
+  const [shouldBeMounted, setShouldBeMounted] = useState(show)
+  const [shouldBeVisible, setShouldBeVisible] = useState(show)
+
+  useEffect(() => {
+    if (show && !shouldBeMounted) {
+      setShouldBeMounted(true)
+    } else if (!show) {
+      setShouldBeVisible(false)
+    }
+  }, [show])
+
+  if (!shouldBeMounted) {
+    return null
+  }
+  return cloneElement(children, {
+    ref: (elem: HTMLElement) => {
+      if (show && elem && !shouldBeVisible) {
+        // force browser to layout so CSS transition will trigger
+        elem.clientHeight
+        setShouldBeVisible(true)
+      }
+    },
     style: {
       transitionProperty: "opacity, transform",
       transformOrigin: "top center",
       ...children.props.style,
-      ...(show ? visible : hidden),
+      ...(shouldBeVisible ? visible : hidden),
+    },
+    onTransitionend: () => {
+      if (!show) {
+        setShouldBeMounted(false)
+      }
     },
   })
+}
